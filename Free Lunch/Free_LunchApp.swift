5d2c2f05e21159301da9f4f0baac778fb4643823
David Olagookun <erijesudo@gmail.com>
2023-09-19 18:59:32 +0100

    Initial Commit

---
+++ Free Lunch/Free_LunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Free_LunchApp.swift
+//  Free Lunch
+//
+//  Created by David OH on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Free_LunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
