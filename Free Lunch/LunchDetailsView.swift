15660279420a63defefe393de58b1e835a87a7b9
tesddev <tesleem.amuda@gmail.com>
2023-09-19 21:36:07 +0100

    add new view

---
+++ Free Lunch/LunchDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  LunchDetailsView.swift
+//  Free Lunch
+//
+//  Created by GIGL iOS on 19/09/2023.
+//
+
+import SwiftUI
+
+struct LunchDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LunchDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        LunchDetailsView()
+    }
+}
