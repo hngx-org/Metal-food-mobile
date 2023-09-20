43e6426f32d0990626563b6745083e57d49bea77
tesddev <tesleem.amuda@gmail.com>
2023-09-20 13:00:54 +0100

    add new swift files

---
+++ Free Lunch/TesViews/RewardSuccessAndSendAnotherView.swift
@@ -0,0 +1,20 @@
+//
+//  RewardSuccessAndSendAnotherView.swift
+//  Free Lunch
+//
+//  Created by Tes on 20/09/2023.
+//
+
+import SwiftUI
+
+struct RewardSuccessAndSendAnotherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RewardSuccessAndSendAnotherView_Previews: PreviewProvider {
+    static var previews: some View {
+        RewardSuccessAndSendAnotherView()
+    }
+}
---
+++ Free Lunch/TesViews/RewardSuccessOnlyView.swift
@@ -0,0 +1,20 @@
+//
+//  RewardSuccessOnlyView.swift
+//  Free Lunch
+//
+//  Created by Tes on 20/09/2023.
+//
+
+import SwiftUI
+
+struct RewardSuccessOnlyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RewardSuccessOnlyView_Previews: PreviewProvider {
+    static var previews: some View {
+        RewardSuccessOnlyView()
+    }
+}
