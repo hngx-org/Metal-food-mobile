e3ca30be2b117e697da5a04354020e158813033f
tesddev <tesleem.amuda@gmail.com>
2023-09-25 00:22:44 +0100

    separate payload from response

--- Free Lunch/Models/PayloadModel/LoginPayload.swift
+++ Free Lunch/Models/PayloadModel/LoginPayload.swift
@@ -6,18 +6,8 @@
 //
 
 import Foundation
-struct logins: Codable {
-    let data: LoginResponse?
-}
+
 struct LoginPayload: Codable {
     var email: String = ""
     var password: String = ""
-}
-
-///testing without codingKeys
-struct LoginResponse: Codable, Equatable{
-    let accessToken : String?
-    let email : String?
-    let orgId : Int?
-    let isAdmin : Int?
 }
